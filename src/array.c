c1d2b7278853f4a1293b75c3a384f3171bbffac2
Wang Keyu <wkykeyu@gmail.com>
2020-07-05 18:16:22 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -11,7 +11,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+    lb = -1;
+    ub = n;
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (A[mid] >= k) ub = mid;
+        else lb = mid;
+    }
+    printf("%d\n", ub);
 
 
   return 0;
